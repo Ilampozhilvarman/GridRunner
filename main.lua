e4a035872e5d62e548c55837f5761ed32e91dc6e
Ilampozhilvarman <Ilampozhilvarman@gmail.com>
2026-07-20 13:17:57 -0500

    Grid added.

--- main.lua
+++ main.lua
@@ -13,18 +13,39 @@ function love.load()
         x = game.middle.x,
         y = game.middle.y,
         speed = 20,
-        jumpKey = "lshift"
+        jumpKey = "space",
+        leftKey = "a",
+        rightKey = "d",
     }
     grid = {
         width = love.graphics.getWidth(),
         height = love.graphics.getHeight(),
-        CellSize = {
-            radius = 30
-        },
+        cellSize = 30,
         cells = {}
     }
+    grid.cols = math.floor(grid.width / grid.cellSize)
+    grid.rows = math.floor(grid.height / grid.cellSize)
+    for i = 0, grid.cols - 1 do
+            grid.cells[i] = grid.cells[i] or {}
+            for j = 0, grid.rows - 1 do
+                grid.cells[i][j] = {
+                    x = i * grid.cellSize,
+                    y = j * grid.cellSize,
+                    width = grid.cellSize,
+                    height = grid.cellSize,
+                    on = true
+                }
+            end
+        end
     print(grid.width)
     print(grid.height)
+end
+
+local function checkCollision(x1, y1, w1, h1, x2, y2, w2, h2)
+    return x1 < x2 + w2 and
+        x1 + w1 > x2 and
+        y1 < y2 + h2 and
+        y1 + h1 > y2
 end
 
 function love.update(dt)
@@ -32,9 +53,11 @@ function love.update(dt)
 end
 
 function love.draw()
-    for i = 1, grid.width / grid.CellSize.radius do
-        for j = 1, grid.height / grid.CellSize.radius do
-            love.graphics.rectangle("line", i * grid.CellSize.radius, j * grid.CellSize.radius, grid.CellSize.radius, grid.CellSize.radius)
+    for i = 0, grid.cols - 1 do
+        for j = 0, grid.rows - 1 do
+            if grid.cells[i][j].on then
+                love.graphics.rectangle("line", grid.cells[i][j].x, grid.cells[i][j].y, grid.cellSize, grid.cellSize)
+            end
         end
     end
 end
